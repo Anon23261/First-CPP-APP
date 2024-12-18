1085470fbcc07aad3d7365a9261772653e3c601d
Anon23261 <Anon23261@users.noreply.github.com>
2024-12-18 00:14:03 +0000

    deploy: d27f991221341013278a982e853ddd60cab335d5

--- fix_doctype.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Find all HTML files in the docs/html directory and fix their DOCTYPE
-find docs/html -name "*.html" -type f -exec sed -i '1i<!DOCTYPE html>' {} \;
-find docs/html -name "*.html" -type f -exec sed -i '1,2{/^$/d}' {} \;
